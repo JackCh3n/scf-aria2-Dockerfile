551d5b815f837f2026299ce2d5aed614ef6a8846
jackChen <1272016935@qq.com>
2022-07-01 11:35:11 +0800

    aria2p

--- app.py
+++ app.py
@@ -1,4 +1,18 @@
 from flask import Flask, request
+import aria2p
+
+# initialization, these are the default values
+aria2 = aria2p.API(
+    aria2p.Client(
+        host="http://127.0.0.1",
+        port=6800,
+        secret="Python_No.1"
+    )
+)
+
+# list downloads
+downloads = aria2.get_downloads()
+print(downloads)
 app = Flask(__name__)
 
 @app.route('/event-invoke', methods = ['POST'])
